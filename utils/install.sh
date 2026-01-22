4669788129623362a1e7e57ec2426a9241195935
Mikhail Onipchenko <monipchenko@tradingview.com>
2026-01-22 15:08:20 +0300

    improve install

--- utils/install.sh
+++ utils/install.sh
@@ -3,9 +3,14 @@ set -euo pipefail
 LANG=C LC_ALL=C
 
 SCRIPT_URL="https://raw.githubusercontent.com/mishanaverno/xray-configurator/main/utils/xr-conf.sh"
-SCRIPT_PATH="/usr/local/bin/xr-conf"
+SCRIPT_PATH="/$HOME/.local/bin/xr-conf"
 
 echo "Downloading xr-conf utility from GitHub..."
+
+mkdir -p "$HOME/.local/bin"
+export PATH="$HOME/.local/bin:$PATH"
+source $HOME/.bashrc
+
 rm -f "$SCRIPT_PATH"
 curl -fsSL "$SCRIPT_URL" -o "$SCRIPT_PATH"
 chmod +x "$SCRIPT_PATH"
